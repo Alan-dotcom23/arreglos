930cef0b77a16cfb53fc570e5378be092918d437
Alan-dotcom23 <bastianmv123@gmail.com>
2025-04-21 13:30:16 -0400

    avance de la clase de arreglos

--- arreglos.py
+++ arreglos.py
@@ -1,1 +1,16 @@
 #Hola, soy un comentario 
+# ¿Que es un arreglo?
+frutas = ["manzana", "pera", "uva",]
+#frutas es un arreglo de 3 elementos, cada elemento es una cadena de texto
+#frutas[0] = "manzana"
+#frutas[1] = "pera"
+#frutas[2] = "uva"
+
+estudiantes = [
+    { "Nombre" : "Juan", "edad": 20, "curso": "3A"},
+    { "Nombre" : "Pedro", "edad": 22, "curso": "3B"},
+    { "Nombre" : "Maria", "edad": 21, "curso": "3A"},
+    { "Nombre" : "Ana", "edad": 23, "curso": "3C"},
+    { "Nombre" : "Luis", "edad": 24, "curso": "3D"},
+]
+print(estudiantes[0]["Nombre"])
